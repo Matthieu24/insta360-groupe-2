37dd65b4628c1f4868e1998fba311ed00ad23dc6
DreamStealerZ <102115730+DreamStealerZ@users.noreply.github.com>
2022-06-27 09:45:24 +1100

    add 3rd infospot

--- js/js.équipe2 copy.js
+++ js/js.équipe2 copy.js
@@ -1,16 +1,14 @@
 var field = true;
-const panorama = new PANOLENS.ImagePanorama('img/av-deck.jpg');
-const otherpic = new PANOLENS.ImagePanorama('img/deck.jpg');
-const viewer = new PANOLENS.Viewer({ output: 'console' });
+const otherpic2 = new PANOLENS.ImagePanorama('img/escalier.jpg');
 
-const theta2 = Math.PI / 1000; // hauteur -pi/2 à pi/2
-const phi2 = Math.PI - 1.6; // placement sur le cercle à plat - pi à pi, 0 = derrière, pi et -pi devant
+const theta = Math.PI / 1000; // hauteur -pi/2 à pi/2
+const phi = Math.PI - 0.1; // placement sur le cercle à plat - pi à pi, 0 = derrière, pi et -pi devant
 var rayon = panorama.radius;
 console.log("Rayon : " + rayon);
 var position = new THREE.Vector3(
-    rayon * Math.cos(theta2) * Math.sin(phi2), // Attention Y sphère
-    rayon * Math.sin(theta2), // Attention Z de la sphère
-    rayon * Math.cos(theta2) * Math.cos(phi2) // Attention X de la sphère
+    rayon * Math.cos(theta) * Math.sin(phi), // Attention Y sphère
+    rayon * Math.sin(theta), // Attention Z de la sphère
+    rayon * Math.cos(theta) * Math.cos(phi) // Attention X de la sphère
 );
 
 var taille = 300;
@@ -23,13 +21,13 @@ var otherinfo = new PANOLENS.Infospot(taille * position.length() / rayon, PANOLE
 otherinfo.position.copy(position);
 //        otherinfo.addHoverText( 'Infospot1');
 otherinfo.addEventListener('click', onFocus);
-otherpic.add(otherinfo);
+otherpic2.add(otherinfo);
 //auttre image
 var otherinfo = new PANOLENS.Infospot(taille * position.length() / rayon, PANOLENS.DataImage.Info);
 otherinfo.position.copy(position);
 //        otherinfo.addHoverText( 'Infospot1');
 otherinfo.addEventListener('click', onFocus);
-otherpic.add(otherinfo);
+otherpic2.add(otherinfo);
 /////////////////////////////////
 viewer.add(panorama);
 viewer.render.sortObjects = true;
@@ -39,10 +37,10 @@ function onFocus() {
     console.log("Clic sur Infospot");
     if (field) {
         viewer.remove(panorama);
-        viewer.add(otherpic);
-        viewer.setPanorama(otherpic);
+        viewer.add(otherpic2);
+        viewer.setPanorama(otherpic2);
     } else {
-        viewer.remove(otherpic);
+        viewer.remove(otherpic2);
         viewer.add(panorama);
         viewer.setPanorama(panorama);
     }
